fea4952c94e71eae35e5a9d963bbf2ea7672a0d6
James Sampson-Brindley <33633047+leafyxr@users.noreply.github.com>
2018-02-25 19:45:33 +0000

    First Update
    Added all the core files

---
+++ Semicolon/Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float fMaxSpeed = 7;
+    public float fJumpSpeed = 7;
+    public int iNumberOfJumpsAllowed = 2;
+    private int iNumberOfJumpsTaken = 0;
+    public int iCollectables = 0;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private bool bJump = true;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                iNumberOfJumpsTaken = 0;
+                velocity.y = fJumpSpeed;
+                Debug.Log("Jumps Taken " + iNumberOfJumpsTaken + " Jumps Allowed " + iNumberOfJumpsAllowed);
+                iNumberOfJumpsTaken++;
+                bJump = iNumberOfJumpsAllowed > iNumberOfJumpsTaken;
+            }
+            else if (bJump)
+            {
+                velocity.y = (0.8f*fJumpSpeed);
+                Debug.Log("Jumps Taken " + iNumberOfJumpsTaken + " Jumps Allowed " + iNumberOfJumpsAllowed);
+                iNumberOfJumpsTaken++;
+                bJump = iNumberOfJumpsAllowed > iNumberOfJumpsTaken;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / fMaxSpeed);
+
+        targetVelocity = move * fMaxSpeed;
+    }
+}
+
